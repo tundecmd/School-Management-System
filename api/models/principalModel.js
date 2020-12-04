c05c06a39824d612530d99ce5cc5ef94c0b2168d
Adepoju Lateef <ademustexcel@gmail.com>
2020-12-04 10:21:18 +0100

    added principal schema, model and routes. also added manager schema and model

---
+++ api/models/principalModel.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const Schema = mongoose.Schema;
+const principalSchema = new Schema({
+    firstname: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    lastname: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true,
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Email is invalid')
+            }
+        }
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    gender: {
+        type: String,
+        required: true
+    }, 
+    age: {
+        type: Number,
+        required: true,
+    },
+    salary: {
+        type: String,
+        required: true
+    },
+    teachers: [String],
+    // classId,
+    // joinedAt
+})
+const Principal = mongoose.model('Principal', principalSchema);
+
+module.exports = Principal;
